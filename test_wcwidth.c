d5f0cdd9134c86be235d50d371fc3a886969d83a
PitWD <pit@lets.de>
2023-10-13 09:25:01 +0200

    Speed test added

--- test_wcwidth.c
+++ test_wcwidth.c
@@ -1,11 +1,11 @@
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdio.h>
-
+#include <time.h>
 #include "wcwidth.h"
 
-static int tests_run;
-static int test_failures;
+static int tests_run = 0;
+static int test_failures = 0;
 
 void assertWidthIs(int expected_width, wchar_t c) {
 	tests_run++;
@@ -16,8 +16,49 @@ void assertWidthIs(int expected_width, wchar_t c) {
         }
 }
 
+unsigned char randomByte() {
+    // Seed the random number generator with the current time
+    srand(time(NULL));
+    
+    // Generate a random number between 0 and 255 (inclusive)
+    int randomNum = rand() % 256;
+
+    // Convert the random number to an unsigned char
+    unsigned char randomByte = (unsigned char)randomNum;
+
+    return randomByte;
+}
+
+void runTimeTest(void){
+	wchar_t testChar[1000000];
+	clock_t startTime;
+	clock_t stopTime;
+	size_t i;
+	printf("\nStart-Randomizer:%f\n", (float)clock()/CLOCKS_PER_SEC);
+	for (i = 0; i < 1000000; i++){
+		testChar[i] = randomByte();
+		testChar[i] *= 256;
+	}
+	for (i = 0; i < 1000000; i++){
+		testChar[i] += randomByte();
+	}
+	printf("Stop-Randomizer:%f\n", (float)clock()/CLOCKS_PER_SEC);
+
+	startTime = clock();
+	for (i = 0; i < 1000000; i++){
+		for (size_t j = 0; j < 10; j++){
+			wcwidth(testChar[i]);
+		}
+	}
+	stopTime = clock();
+	printf("\nStart wcwidth():%f\n", (float)startTime/CLOCKS_PER_SEC);
+	printf("Stop wcwidth():%f\n", (float)stopTime/CLOCKS_PER_SEC);
+	printf("%lu wcwidth() / Second\n", (unsigned long)(10000000/((float)(stopTime-startTime)/CLOCKS_PER_SEC)));
+
+}
+
 int main() {
-        assertWidthIs(1, 'a');
+        assertWidthIs(1, L'a');
         assertWidthIs(1, L'ö');
 
 	// Some wide:
@@ -38,6 +79,7 @@ int main() {
 
 	if (test_failures > 0) printf("%d tests FAILED, ", test_failures);
 	printf("%d tests OK\n", tests_run);
+	runTimeTest();
 	return (test_failures == 0) ? 0 : 1;
 }
 
